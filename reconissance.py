03e16b4782a594741ce37af9d961272a903effa6
techathena <41319133+techathena@users.noreply.github.com>
2020-05-26 11:42:12 +0530

    Update reconissance.py

--- reconissance.py
+++ reconissance.py
@@ -3,20 +3,21 @@ import requests
 import socket
 import json
 
-
-if len(sys.argv)<2:
+print("enter the name of the site : ")
+site=input()
+if len(site)<2:
  print("use:"+sys.argv[0]+" <add_url>")
  sys.exit(1)
+else:
+  recv = requests.get("https://"+site)
+  print("\n"+str(recv.headers))
 
-recv = requests.get("https://"+sys.argv[1])
-print("\n"+str(recv.headers))
-
-gethost = socket.gethostbyname(sys.argv[1])
-print("/n the ip address of requested site is "+ gethost)
-#ipinfo.io
-recv2 = requests.get("https://ipinfo.io/"+gethost+"/json")
-rep = json.loads(recv2.text)
-print("location: "+ rep['locn'])
-print("region :"+rep['region'])
-print("city :"+rep["city"])
-print("country :"+rep["country"])
+  gethost = socket.gethostbyname(site)
+  print("/n the ip address of requested site is "+ gethost)
+  #ipinfo.io
+  recv2 = requests.get("https://ipinfo.io/"+gethost+"/json")
+  rep = json.loads(recv2.text)
+  print("location: "+ rep['locn'])
+  print("region :"+rep['region'])
+  print("city :"+rep["city"])
+  print("country :"+rep["country"])
