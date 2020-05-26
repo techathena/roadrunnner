587166816332f0f310da014cb38e98e493cb585d
techathena <41319133+techathena@users.noreply.github.com>
2020-05-26 11:39:16 +0530

    Update basicportscan.py

--- basicportscan.py
+++ basicportscan.py
@@ -1,6 +1,8 @@
 import sys
 import nmap
-target = str(sys.argv[1])
+print("enter the target")
+
+target = input()
 ports = [21,22,80,139,443,8080]
 
 scan_var= nmap.PortScanner()
@@ -9,4 +11,3 @@ print("\n scanning"+target+"for the ports 21,22,80,139,443,8080")
 for port in ports:
     portscan =scan_var.scan(target,str(port))
     print (portscan)
-
